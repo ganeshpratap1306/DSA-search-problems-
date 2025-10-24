a792a0fe037ce47c4badf11cfb0fb87ff7956fc5
Ganesh Pratap Vishwakarma <ganeshpratapvishwakarma05@gmail.com>
2025-10-24 19:30:11 +0530

    final for deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const fs = require("fs");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const path = require('path');
 
 app.set("view engine", "ejs");
